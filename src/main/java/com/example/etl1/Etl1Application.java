588fb3667ce414f023d0f22c259f849852d7dccc
Tim Charteris <tim.charterii@gmail.com>
2025-08-05 13:08:04 +0100

    intial commit

---
+++ src/main/java/com/example/etl1/Etl1Application.java
@@ -0,0 +1,13 @@
+package com.example.etl1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Etl1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Etl1Application.class, args);
+	}
+
+}
