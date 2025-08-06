c82aa94615b0af7bbadc8d28c682938b1bb30306
Sean McCaughey <sean.mccaughey@protonmail.com>
2025-08-06 15:06:17 +0100

    Removed unnecessary template and updated root.

--- src/main/java/com/example/etl1/Controllers/ComponentsController.java
+++ src/main/java/com/example/etl1/Controllers/ComponentsController.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
 import java.io.IOException;
@@ -53,8 +52,8 @@ public class ComponentsController {
     @Autowired
     PowerSupplyRepository powerSupplyRepository;
 
-    @GetMapping("/parts")
-    public ModelAndView populateComponentsData() throws IOException {
+    @GetMapping("/load-component-data")
+    public void populateComponentsData() throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         Case[] cases = mapper.readValue(new File(System.getProperty("user.dir") + "/src/main/resources/PC_Components_Data/case.json"), Case[].class);
         CPU[] cpus = mapper.readValue(new File(System.getProperty("user.dir") + "/src/main/resources/PC_Components_Data/cpu.json"), CPU[].class);
@@ -64,9 +63,7 @@ public class ComponentsController {
         Memory[] memory = mapper.readValue(new File(System.getProperty("user.dir") + "/src/main/resources/PC_Components_Data/memory.json"), Memory[].class);
         Motherboard[] motherboards = mapper.readValue(new File(System.getProperty("user.dir") + "/src/main/resources/PC_Components_Data/motherboard.json"), Motherboard[].class);
         PowerSupply[] powerSupplies = mapper.readValue(new File(System.getProperty("user.dir") + "/src/main/resources/PC_Components_Data/power-supply.json"), PowerSupply[].class);
-        ModelAndView modelAndView = new ModelAndView("/parts");
 
-        modelAndView.addObject("cpuName", cpuCoolers[0].getRpm().getFirst().getRpm());
         caseRepository.saveAll(Arrays.asList(cases));
         cpuRepository.saveAll(Arrays.asList(cpus));
 
@@ -132,7 +129,5 @@ public class ComponentsController {
 
         motherboardRepository.saveAll(Arrays.asList(motherboards));
         powerSupplyRepository.saveAll(Arrays.asList(powerSupplies));
-
-        return modelAndView;
     }
 }
