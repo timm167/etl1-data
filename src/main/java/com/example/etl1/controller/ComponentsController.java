e703a767016fd795c7600879cdbe772dd20b5e09
Jamie Wolfenden <jamiewolfenden@gmail.com>
2025-08-11 14:20:01 +0100

    Cleaned up commented code

--- src/main/java/com/example/etl1/controller/ComponentsController.java
+++ src/main/java/com/example/etl1/controller/ComponentsController.java
@@ -293,35 +293,6 @@ public class ComponentsController {
         return modelAndView;
     }
 
-//    public static Sort getSortMethod(String sortBy, String order) {
-//        Sort.Direction direction;
-//
-//        if (sortBy != null) {
-//            if (order != null && order.equals("Descending")) {
-//                direction = Sort.Direction.DESC;
-//            } else {
-//                direction = Sort.Direction.ASC;
-//            }
-//
-//            String property = switch (sortBy) {
-//                case "Name" -> "name";
-//                case "Price" -> "price";
-//                case "Size" -> "externalVolume";
-//                case "Core Clock" -> "coreClock";
-//                case "Capacity" -> "capacity";
-//                case "Speed" -> "speed.speed";
-//                case "Wattage" -> "wattage";
-//                default -> null;
-//            };
-//
-//            if (property != null) {
-//                return Sort.by(direction, property);
-//            }
-//        }
-//
-//        return null;
-//    }
-
     private void convertComponentPriceToPounds(Component[] components) {
         for (Component component : components) {
             component.setPrice(component.getPrice() * 0.75);
