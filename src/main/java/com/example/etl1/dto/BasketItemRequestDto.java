8139468c9011aab87e47fd7f26a6a2dd443170b6
Natt Speight <NattSpeight@gmail.com>
2025-08-14 11:08:21 +0100

    Fixed post merge issues

--- src/main/java/com/example/etl1/dto/BasketItemRequestDto.java
+++ src/main/java/com/example/etl1/dto/BasketItemRequestDto.java
@@ -1,4 +1,4 @@
-package com.example.etl1.repository;
+package com.example.etl1.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
--- src/main/resources/db/migration/V15__create_basket_items_table.sql
+++ src/main/resources/db/migration/V15__create_basket_items_table.sql
@@ -3,6 +3,6 @@ CREATE TABLE basket_items (
      basket_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
     quantity INTEGER NOT NULL,
-    FOREIGN KEY (basket_id) REFERENCES basket(id) ON DELETE CASCADE,
+    FOREIGN KEY (basket_id) REFERENCES baskets(id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
